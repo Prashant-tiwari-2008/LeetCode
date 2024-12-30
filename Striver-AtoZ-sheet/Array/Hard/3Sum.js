76057a49f4af1baad726bf318dcfae4a0fa272d4
Prashant tiwari <prashanttiwari.vns@gmail.com>
2024-12-30 07:06:20 +0530

    three sum problem

--- Striver-AtoZ-sheet/Array/Hard/3Sum.js
+++ Striver-AtoZ-sheet/Array/Hard/3Sum.js
@@ -3,3 +3,49 @@ console.log("three sum problem");
 
 // Notice that the solution set must not contain duplicate triplets.
 
+let nums = [-1, 0, 1, 2, -1, -4] // [[-1,-1,2],[-1,0,1]]
+let nums1 = [0, 1, 1] // []
+
+var threeSum = (nums) => {
+    const result = [];
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        if(i > 0 && nums[])
+    }
+}
+
+console.log(twoSum(nums))
+console.log(twoSum(nums1))
+
+var threeSum = function(nums) {
+    const result = [];
+    nums.sort((a, b) => a - b); // Sort the array
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue; // Skip duplicates for the first element
+
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
+            if (sum === 0) {
+                result.push([nums[i], nums[left], nums[right]]);
+                left++;
+                right--;
+
+                // Skip duplicates for the second and third elements
+                while (left < right && nums[left] === nums[left - 1]) left++;
+                while (left < right && nums[right] === nums[right + 1]) right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return result;
+};

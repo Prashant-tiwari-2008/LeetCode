21adecd6de094b7d510765a5cf39ac745f985e6d
Prashant tiwari <prashanttiwari.vns@gmail.com>
2025-01-01 08:19:28 +0530

    1st jan 2025

--- Striver-AtoZ-sheet/Array/Hard/Merge_Intervals.js
+++ Striver-AtoZ-sheet/Array/Hard/Merge_Intervals.js
@@ -6,16 +6,12 @@ let intervals2 = [[1, 4], [4, 5]] //  [[1,5]]
 
 var merge = (intervals) => {
     let result = [];
+    intervals.sort((a, b) => a[0] - b[0]);
     for (let i = 0; i < intervals.length; i++) {
-        if (intervals[i + 1]) {
-            if (intervals[i + 1][0] <= intervals[i][1]) {
-                result.push([intervals[i][0], intervals[i + 1][1]])
-                i++;
-            } else {
-                result.push(intervals[i])
-            }
-        }else{
+        if (result.length == 0 || result[result.length - 1][1] < intervals[i][0]) {
             result.push(intervals[i])
+        } else {
+            result[result.length - 1][1] = Math.max(result[result.length - 1][1], intervals[i][1])
         }
     }
     return result;
---
+++ Striver-AtoZ-sheet/Array/Hard/missing_and_repeating.js
@@ -0,0 +1,20 @@
+// Given an unsorted array arr of positive integers. One number a from the set [1, 2,....,n] is missing and one number b occurs twice in the array. Find numbers a and b.
+
+// Note: The test cases are generated such that there always exists one missing and one repeating number within the range [1,n].
+
+let arr1 = [2, 2] // [2,1]
+let arr2 = [1, 3, 3] // [3,2]
+let arr3 = [4, 3, 6, 2, 1, 1] // [1,5]
+
+function findTwoElement(arr) {
+    arr.sort((a, b) => a - b)
+    let missingNumber;
+    let repeatedNumber;
+    for (let i = 0; i < arr.length; i++) {
+
+    }
+}
+
+console.log(findTwoElement(arr1))
+console.log(findTwoElement(arr2))
+console.log(findTwoElement(arr3))
